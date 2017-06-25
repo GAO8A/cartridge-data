446a8983d09b81196b397e45ee231424cae729bc
Rick <rikgao@icloud.com>
2017-06-25 15:53:39 -0400

    done cartridge

--- app.js
+++ app.js
@@ -8,24 +8,33 @@ let eA = '35-10',
 	eL = '0.89-0.25',
 	eD = '8.81-0.51';
 
-let bD = '9.931',
-	bL = '5.08';
-
-let bodyLength = '12.70';
+let bulletD = '9.030-0.076';
+let baseL = '5.08';
+let bodyL = '12.70';
 
 let sD = '9.680';
 
 let nD = '9.652';
 
-let cL = '19.15-0.25';
+let caseL = '19.15-0.25',
+    caseD = '9.931';
 
-let diameters = [hD, eD, bD, sD, nD];
+let cMin = '25.40',
+	cMax = '29.69',
+	catridgeLength = parseLength(parseCatridgeLength(cMin, cMax));
 
-let lengths = [hL, rL, eL, cL, bodyLength];
+let diameters = [hD, eD, caseD, sD, nD, bulletD];
+
+let lengths = [hL, rL, eL, caseL, bodyL, baseL];
 
 let angles = [rA, eA];
 
-function parseLength (length) {
+///
+
+let cartridgeData;
+let bulletData;
+
+function parseLength (length, secondLength) {
 	let range;
 	let value;
 	if (length.includes('-')) {
@@ -41,77 +50,105 @@ function parseLength (length) {
 	return value;
 }
 
+function parseCatridgeLength(cmin, cmax) {
+	const diff = (cmax - cmin)/2;
+	return "" + cmax + "-" + diff + "";
+
+}
+
 function parseAngle (ang) {
-	
 	return (ang / 180) * Math.PI;
 }
 
-function buildCartridge(d,l,a) {
-	[hD, eD, bD, sD, nD] = d.map(parseLength).map((i)=> i/2);
-	[hL, rL, eL, cL, bodyLength] = l.map(parseLength);
+function buildCartridge(d,l,a,cmin,cmax) {
+	[hD, eD, caseD, sD, nD, bulletD] = d.map(parseLength).map((i)=> i/2);
+	[hL, rL, eL, caseL, bodyL, baseL] = l.map(parseLength);
 	[rA, eA] = a.map(parseLength).map(parseAngle);
 
-	return buildHead(hL,hD,rL,rA,eD,eL,bD,eA,bodyLength,sD, cL, nD);
+	cartridgeData = buildCase(hL,hD,rL,rA,eD,eL,caseD,eA,bodyL,sD, caseL, nD ,baseL);
+	bulletData = buildBullet(caseD,nD,sD,caseL, cmin, cmax, bulletD);
 }
 
-function buildHead(hl,hd,rl,ra,ed,el,bd,ea, bodylength, sD, cL, nD) {
+function buildBullet(cd, nd, sd, cl, cmin, cmax, bd) {
+	const bulletMidLen = (catridgeLength - cl)/0.95;
+	return [
+			[0, cd-nd],
+			[bulletMidLen, cd-bd],
+			[catridgeLength-cl, cd]
+	];
+
+}
+
+function buildCase(hl,hd,rl,ra,ed,el,cd,ea, bodylength, sd, cl, nd, bl) {
 
 	let rimAngle = calcRimAngle(hl, rl, ra);
-	let ejecAngle = calcEjecAngle(bd, ed, ea);
+	let ejecAngle = calcEjecAngle(cd, ed, ea);
 
  return [
- 			[0, bd], // rim start
+ 			[0, cd], // rim start
  			[0, rimAngle],
  			[hl-rl,0],
- 			[hl-rl,bd],
+ 			[hl-rl,cd],
  			[hl-rl,0],
  			[hl,0],
- 			[hl, bd],
- 			[hl, bd-ed], // extractor start
- 			[hl+el, bd-ed],
- 			[hl+el, bd],
- 			[hl+el, bd-ed],
+ 			[hl, cd],
+ 			[hl, cd-ed], // extractor start
+ 			[hl+el, cd-ed],
+ 			[hl+el, cd],
+ 			[hl+el, cd-ed],
  			[ejecAngle+hl+el, 0], // might be wrong
- 			[ejecAngle+hl+el, bd],
+ 			[ejecAngle+hl+el, cd],
  			[ejecAngle+hl+el, 0], // body start
- 			[bodylength, bd-sD],
- 			[cL, bd-nD],
- 			[cL , bd],
- 			[cL , bd-nD] // bullet start
+			[bl, 0], // base length? might wrong
+ 			[bodylength, cd-sd],
+ 			[cl, cd-nd],
+ 			[cl , cd],
+ 			[cl , cd-nd] // bullet start
  		];
 }
 
 function calcRimAngle(hl,rl,ra) {
-	// console.log('ra: ', (hl-rl)/(Math.tan(ra)));
 	return (hl-rl)/(Math.tan(ra));
 }
 
-function calcEjecAngle(bd, ed, ea) {
-	let foo = (bd-ed)/(Math.tan(ea));
-	console.log(foo);
-	return foo;
+function calcEjecAngle(cd, ed, ea) {
+	return (cd-ed)/(Math.tan(ea));
 }
 
-let data = buildCartridge(diameters,lengths, angles);
+let foo = buildCartridge(diameters,lengths, angles,cMin, cMax);
 
-let round = d3.select('.cartridge')
+let roundGenerator = d3.line();
+let roundPathString = roundGenerator(cartridgeData);
+
+let bulletGenerator = d3.line().curve(d3.curveBundle);
+let bulletPathString = bulletGenerator(bulletData);
+
+let casing = d3.select('.cartridge')
 			  .append('svg')
-			  .attr('height',  "100%")
-			  .attr('width', "100%")
+			  .attr('class', 'case1')
+			  .attr('height',  ""+hD*20+"px")
+			  .attr('width', ""+caseL*10+"px")
 			  .append('path')
-			  .attr('class', 'round1')
-			  .attr("transform","scale(10)");
+			  .attr("transform","scale(10)")
+			  .attr('d', roundPathString);;
 
-let round2 = d3.select('svg')
+let bullet1 = d3.select('.bullet')
+			  .append('svg')
+			  .attr('class', 'bullet1')
+			  .attr('height',  ""+caseD*20+"px")
+			  .attr('width', ""+(catridgeLength-caseL)*10+"px")
 			  .append('path')
-			  .attr('class', 'round2')
-			  .attr("transform","translate(0 "+ hD*20 +") scale(10 -10)");
+			  .attr("transform","scale(10)")
+			  .attr('d', bulletPathString);;
+			  
+let casing2 = d3.selectAll('svg')
+			  .filter('.case1')
+			  .append('path')
+			  .attr("transform","translate(0 "+ hD*20 +") scale(10 -10)")
+			  .attr('d', roundPathString);
 
-let lineGenerator = d3.line();
-let pathString = lineGenerator(data);
-
-d3.select('.round1')
-	.attr('d', pathString);
-
-d3.select('.round2')
-	.attr('d', pathString);
+let bullet2 = d3.selectAll('svg')
+			  .filter('.bullet1')
+			  .append('path')
+			  .attr("transform","translate(0 "+ hD*20 +") scale(10 -10)")
+			  .attr('d', bulletPathString);
