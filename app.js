191b96dfbe78b196665fe4655d7161fa4c6b631e
Rick <rikgao@icloud.com>
2017-09-27 23:04:20 -0400

    added annotations

--- app.js
+++ app.js
@@ -1,40 +1,47 @@
-let hD = '10.01-0.25',
-	hL = '1.27-0.25';
-
-let rA = '35+20',
-	rL = '0.89-0.25';
-
-let eA = '35-10',
-	eL = '0.89-0.25',
-	eD = '8.81-0.51';
-
 let bulletD = '9.030-0.076';
 let baseL = '5.08';
 let bodyL = '12.70';
 
-let sD = '9.680';
+let caseL = '19.15-0.25',
+    caseD = '9.931',
+	cMax = '29.69',
+	cMin = '25.40';
+	
+let eA = '35-10',
+	eL = '0.89-0.25',
+	eD = '8.81-0.51';
+
+let hD = '10.01-0.25',
+	hL = '1.27-0.25';
 
 let nD = '9.652';
 
-let caseL = '19.15-0.25',
-    caseD = '9.931';
+let rA = '35+20',
+	rL = '0.89-0.25';
 
-let cMin = '25.40',
-	cMax = '29.69',
-	catridgeLength = parseLength(parseCatridgeLength(cMin, cMax));
+let sD = '9.680';
 
-let diameters = [hD, eD, caseD, sD, nD, bulletD];
-
-let lengths = [hL, rL, eL, caseL, bodyL, baseL];
+let catridgeLength = parseCatridgeLength(cMin, cMax);
 
 let angles = [rA, eA];
 
-///
+let diameters = [hD, eD, caseD, sD, nD, bulletD];
 
+let lengths = [hL, rL, eL, caseL, bodyL, baseL, catridgeLength];
+
+// Array of coordinates for the cartridge casing
 let cartridgeData;
+
+// Array of coordinates for the example bullet
 let bulletData;
 
-function parseLength (length, secondLength) {
+/* For calculating the average length with tolerances.
+ * Takes in a dimension with tolerances and creates a floor
+ * and ceiling value with tolerances and returns its average
+ * @param String: dimension with +/- tolerances or none
+ * @return Number: Average value with tolerances
+ */
+function parseLength (length) {
 	let range;
 	let value;
 	if (length.includes('-')) {
@@ -50,39 +57,76 @@ function parseLength (length, secondLength) {
 	return value;
 }
 
+/* For calculating specifically the average cartridge
+ * length with the min and max string values 
+ * @param String: Minimum cartridge dimension
+ * @param String: Maximum cartridge dimension
+ * @return Number: Average length of cartridge
+ */
 function parseCatridgeLength(cmin, cmax) {
 	const diff = (cmax - cmin)/2;
 	return "" + cmax + "-" + diff + "";
 
 }
 
+/* For converting angle value into radians.
+ * @param Number: Degree value of angle
+ * @return Number: Radian value of angle
+ */
 function parseAngle (ang) {
-	return (ang / 180) * Math.PI;
+	return ang * Math.PI/180;
 }
 
+/* For building an array of coordinates for both the cartridge
+ * casing and the example bullet. Takes in arrays of dimension types and
+ * converts their string dimension values to numbers. Passes new number 
+ * values to respective buildCase() and buildBullet() 
+ * @param [String]: Array of diameter dimensions
+ * @param [String]: Array of length dimensions
+ * @param [String]: Array of angle dimensions
+ * @param String: Minimum cartridge dimension
+ * @param String: Maximum cartridge dimension
+ * @return Void:
+ */
 function buildCartridge(d,l,a,cmin,cmax) {
 	[hD, eD, caseD, sD, nD, bulletD] = d.map(parseLength).map((i)=> i/2);
-	[hL, rL, eL, caseL, bodyL, baseL] = l.map(parseLength);
+	[hL, rL, eL, caseL, bodyL, baseL, catridgeLength] = l.map(parseLength);
 	[rA, eA] = a.map(parseLength).map(parseAngle);
 
-	cartridgeData = buildCase(hL,hD,rL,rA,eD,eL,caseD,eA,bodyL,sD, caseL, nD ,baseL);
-	bulletData = buildBullet(caseD,nD,sD,caseL, cmin, cmax, bulletD);
+	cartridgeData = buildCase(hL,rL,rA,eD,eL,caseD,eA,bodyL,sD, caseL, nD ,baseL);
+	bulletData = buildBullet(caseD, nD, caseL, bulletD, catridgeLength);
 }
 
-function buildBullet(cd, nd, sd, cl, cmin, cmax, bd) {
-	const bulletMidLen = (catridgeLength - cl)/0.95;
+/* For building array of coordinates for catridge casing.
+ * Takes in various dimensions and builds an array of 
+ * coordinates for d3.line().curve() interpolation.
+ * length with the min and max string values 
+ * @param Number: Case diameter
+ * @param Number: Neck diameter
+ * @param Number: Case length average
+ * @param Number: Bullet diameter average 
+ * @param Number: Cartridge length average
+ * @return [..[Number]]: Array of Coordinates for bullet
+ */
+function buildBullet(cd, nd, cl, bd, catridgeL) {
+	const bulletMidLen = (catridgeL - cl)/0.95;
 	return [
 			[0, cd-nd],
 			[bulletMidLen, cd-bd],
-			[catridgeLength-cl, cd]
+			[catridgeL-cl, cd]
 	];
 
 }
 
-function buildCase(hl,hd,rl,ra,ed,el,cd,ea, bodylength, sd, cl, nd, bl) {
+/* For building array of coordinates for example bullet.
+ * Takes in various dimensions and builds an array of 
+ * coordinates for d3.line() svg line generator.
+ * @return [..[Number]]: Array of Coordinates for casing
+ */
+function buildCase(hl,rl,ra,ed,el,cd,ea, bodylength, sd, cl, nd, bl) {
 
-	let rimAngle = calcRimAngle(hl, rl, ra);
-	let ejecAngle = calcEjecAngle(cd, ed, ea);
+	let rimAngle = calcAngleCoord(hl, rl, ra);
+	let ejecAngle = calcAngleCoord(cd, ed, ea);
 
  return [
  			[0, cd], // rim start
@@ -107,22 +151,29 @@ function buildCase(hl,hd,rl,ra,ed,el,cd,ea, bodylength, sd, cl, nd, bl) {
  		];
 }
 
-function calcRimAngle(hl,rl,ra) {
-	return (hl-rl)/(Math.tan(ra));
+/* For calculating the y coordinate opposite of the angle
+ * Takes in various dimensions and returns coordinate value.
+ * @param Number: Case dimension
+ * @param Number: Case dimension
+ * @param Number: angle in radians
+ * @return Number: Value of y coordinate
+ */
+function calcAngleCoord(dA,dB,a) {
+	return (dA-dB)/(Math.tan(a));
 }
 
-function calcEjecAngle(cd, ed, ea) {
-	return (cd-ed)/(Math.tan(ea));
-}
+// Generates cartridgeData and bulletData coordinate sets
+let cartridge = buildCartridge(diameters,lengths, angles,cMin, cMax);
 
-let foo = buildCartridge(diameters,lengths, angles,cMin, cMax);
-
+// Generates d value for the cartridge casing
 let roundGenerator = d3.line();
 let roundPathString = roundGenerator(cartridgeData);
 
+// Generates d value for the example bullet
 let bulletGenerator = d3.line().curve(d3.curveBundle);
 let bulletPathString = bulletGenerator(bulletData);
 
+// D3 DOM manipulation
 let casing = d3.select('.cartridge')
 			  .append('svg')
 			  .attr('class', 'case1')
